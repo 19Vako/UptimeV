b91b23ded4d141fc23e36eb15555ed2feda21a1d
19Vako <152034949+19Vako@users.noreply.github.com>
2026-07-21 18:04:49 +0300

    first commit

---
+++ src/app/(job)/editJob.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function EditJob() {
+  return (
+    <View>
+      <Text>EditJob</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/app/(job)/jobDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function JobDetails() {
+  return (
+    <View>
+      <Text>JobDetails</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
